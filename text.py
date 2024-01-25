8dae3465e712200c18ba3b881166d66918717ec4
rajabboyeva04 <rajabboyevamuxlisa707@gmail.com>
2024-01-25 12:02:56 +0300

    Fayllar

---
+++ text.py
@@ -0,0 +1,46 @@
+class Kitob:
+    def __init__(self,nomi):
+        self.nomi=nomi
+        with open(f"{self.nomi}","w") as fayl:
+            pass
+        
+        def yozish(self,matn):
+            with open(f"{self.nomi}","w") as fayl:
+                fayl.write(matn)
+                
+    def uqish(self):
+        with open(f"{self.nomi}","r") as fayl:
+            return fayl.read()
+        
+    def add_text(matn):
+        with open(f"{self.nomi}","a") as fayl:
+            fayl.write(matn)
+            
+            
+    def first_row():
+        with open(f"{self.nomi}","r") as fayl:
+            return fayl.readline()
+        
+    
+    def massiv(self):
+        print(self.massivv)        
+
+    def __repr__ (self):
+        with open(f"{self.nomi}","r") as fayl:
+            return self.nomi
+        
+        def __getitem__():
+            
+            
+        
+    
+        
+        
+file=Kitob("myfile.txt") 
+file.yozish("Kelajak") 
+print(file.uqish)      
+        
+                    
+                
+                    
+                    
